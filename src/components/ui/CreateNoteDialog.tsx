e7ec5bb053808b923b2c73dfd9d7585c92f38829
salvadorK <karlanthonysalvador@gmail.com>
2024-01-09 22:58:01 -0500

    update on Tiptap

--- src/components/ui/CreateNoteDialog.tsx
+++ src/components/ui/CreateNoteDialog.tsx
@@ -1,11 +1,47 @@
 'use client'
 import React from 'react'
-import { Dialog, DialogContent ,DialogHeader,DialogTrigger} from './dialog'
-import { Plus } from 'lucide-react'
+import { Dialog, DialogContent ,DialogDescription,DialogHeader,DialogTitle,DialogTrigger} from './dialog'
+import { Loader2, Plus } from 'lucide-react'
+import { Input } from './input'
+import { Button } from './button'
+import { useMutation } from '@tanstack/react-query'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
 
 type Props = {}
 
 const CreateNoteDialog = (props: Props) => {
+const router = useRouter()
+const [input, setInput] = React.useState('')
+
+const createNoteBook = useMutation({
+    mutationFn:async () => {
+        const response = await axios.post('/api/createNoteBook', {
+            name:input
+        })
+        return response.data
+    }
+})
+
+const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if(input === '') {
+        window.alert('Please enter a name for your notebook')
+        return
+    }
+    createNoteBook.mutate(undefined, {
+        onSuccess: ({note_id}) => {
+            console.log("created note id", note_id)
+            router.push(`/notebook/${note_id}`)
+        },
+        onError: (error) => {
+            console.log(error)
+        }
+        
+
+    })
+}
+
   return (
     <Dialog>
         <DialogTrigger>
@@ -16,8 +52,24 @@ const CreateNoteDialog = (props: Props) => {
         </DialogTrigger>
         <DialogContent>
             <DialogHeader>
-                
+                <DialogTitle>
+                New Note Book
+                </DialogTitle>
+                <DialogDescription>
+                You can create a new note
+                </DialogDescription>
             </DialogHeader>
+            <form onSubmit={handleSubmit}>
+                <Input value={input} onChange={e => setInput(e.target.value)}placeholder='Name...'/>
+                <div className="h-4"></div>
+                <div className="flex items-center gap-2">
+                    <Button type='reset' variant={'secondary'}>Cancel</Button>
+                    <Button type='submit' className='bg-green-600' disabled={createNoteBook.isPending}>
+                        {createNoteBook.isPending && <Loader2 className='w-4 h-4 mr-2 animate-spin'/>}
+                        Create
+                    </Button>
+                </div>
+            </form>
         </DialogContent>
     </Dialog>
   )
