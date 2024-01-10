6ec78676366729b638c8a8f9e08076b9416b1c2e
salvadorK <karlanthonysalvador@gmail.com>
2024-01-10 16:03:24 -0500

    update on adding AI

--- src/components/TipTapEditor.tsx
+++ src/components/TipTapEditor.tsx
@@ -4,27 +4,66 @@ import { EditorContent, useEditor } from '@tiptap/react'
 import { StarterKit } from '@tiptap/starter-kit'
 import TipTapMenuBar from './TipTapMenuBar'
 import { Button } from './ui/button'
+import { useDebounce } from '@/lib/useDebounce'
+import { useMutation } from '@tanstack/react-query'
+import Text from '@tiptap/extension-text'
+import axios from 'axios'
+import { NoteType } from '@/lib/db/schema'
 
-type Props = {}
+type Props = { note: NoteType}
 
-const TipTapEditor = (props: Props) => {
+const TipTapEditor = ({note}: Props) => {
 
-    const [editorState, setEditorState] = React.useState('')
+    const [editorState, setEditorState] = React.useState(note.editorState || `<h1>${note.name}<h1>`)
+    const saveNote = useMutation({
+        mutationFn: async () => {
+             const response = await axios.post('/api/saveNote', {
+                noteId:note.id, editorState
+             })
+             return response.data
+        }
+
+    })
+
+    const customText = Text.extend({
+        addKeyboardShortcuts() {
+            return {
+                'Shift-a': () => this.editor.commands.toggleBold()
+            }
+        }
+    })
 
     const editor =useEditor({
         autofocus: true,
-        extensions: [StarterKit],
+        extensions: [StarterKit, customText],
         content: editorState,
         onUpdate: ({editor}) => {
         setEditorState(editor.getHTML())
         }
     })
 
+    const debounceEditorState = useDebounce(editorState,500)
+
+    React.useEffect(() => {
+        if (debounceEditorState === '') return
+        saveNote.mutate(undefined, {
+            onSuccess: data => {
+                console.log('success update',data)
+            },
+            onError: error => {
+                console.error(error)
+            }
+        })
+    },[debounceEditorState])
+
+
   return (
         <>
             <div className='flex'>
                 {editor && <TipTapMenuBar editor={editor} />}
-                <Button>Saved</Button>
+                <Button disabled variant='outline'>
+                    {saveNote.isPending ? "Saving..." : "Saved"}
+                </Button>
             </div>
             <div>
                 <EditorContent editor={editor} />
--- src/lib/db/openai.ts
+++ src/lib/db/openai.ts
@@ -1,5 +1,7 @@
 import { Configuration, OpenAIApi} from 'openai-edge'
 
+export const runtime = 'edge';
+
 const config = new Configuration({
     apiKey: process.env.OPENAI_API_KEY
 })
